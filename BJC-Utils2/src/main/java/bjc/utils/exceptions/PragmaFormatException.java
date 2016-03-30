3113ab1a0ee0163b8c254f52bbf5fb1ee67e8466
bculkin2442 <bjculkin@mix.wvu.edu>
2016-03-30 09:27:08 -0400

    New exception for invalid pragma syntax

---
+++ BJC-Utils2/src/main/java/bjc/utils/exceptions/PragmaFormatException.java
@@ -0,0 +1,29 @@
+package bjc.utils.exceptions;
+
+import java.util.InputMismatchException;
+
+/**
+ * The exception to throw whenever a pragma is used with invalid syntax
+ * 
+ * @author ben
+ *
+ */
+public class PragmaFormatException extends InputMismatchException {
+	// Version ID for serialization
+	private static final long serialVersionUID = 1288536477368021069L;
+
+	/**
+	 * Create a new exception with the given message
+	 * @param msg The message to explain why the exception was thrown
+	 */
+	public PragmaFormatException(String msg) {
+		super(msg);
+	}
+	
+	/**
+	 * Create a new exception
+	 */
+	public PragmaFormatException() {
+		super();
+	}
+}
