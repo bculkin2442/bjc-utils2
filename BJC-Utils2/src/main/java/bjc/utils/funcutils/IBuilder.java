a00b34502b11aded74d22a2df72c8751182607da
bculkin2442 <bjculkin@mix.wvu.edu>
2016-03-31 17:39:52 -0400

    Created interface for implementations of builder pattern

---
+++ BJC-Utils2/src/main/java/bjc/utils/funcutils/IBuilder.java
@@ -0,0 +1,32 @@
+package bjc.utils.funcutils;
+
+/**
+ * Generic interface for objects that implement the builder pattern
+ * 
+ * @author ben
+ *
+ * @param <E>
+ *            The type of object being built
+ */
+public interface IBuilder<E> {
+	/**
+	 * Build the object this builder is building
+	 * 
+	 * @return The built object
+	 * @throws IllegalStateException
+	 *             if the data in the builder cannot be built into its
+	 *             corresponding object at this point in time
+	 */
+	public E build();
+
+	/**
+	 * Reset the state of this builder to its initial state
+	 * 
+	 * @throws UnsupportedOperationException
+	 *             if the builder doesn't support resetting its state
+	 */
+	public default void reset() {
+		throw new UnsupportedOperationException(
+				"Builder doesn't support state resetting");
+	}
+}
