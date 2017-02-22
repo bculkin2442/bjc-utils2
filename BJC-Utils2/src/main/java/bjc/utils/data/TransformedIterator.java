620adc2fe00c1337b21f01a93be86729350c433c
bculkin2442 <bjculkin@mix.wvu.edu>
2017-02-22 04:38:14 -0500

    Added transforming iterator

---
+++ BJC-Utils2/src/main/java/bjc/utils/data/TransformedIterator.java
@@ -0,0 +1,23 @@
+package bjc.utils.data;
+
+import java.util.Iterator;
+import java.util.function.Function;
+
+public class TransformedIterator<PreType, PostType> implements Iterator<PostType> {
+	private Iterator<PreType> source;
+
+	private Function<PreType, PostType> transform;
+
+	public TransformedIterator(Iterator<PreType> src, Function<PreType, PostType> trans) {
+		source = src;
+		trans  = transform;
+	}
+
+	public boolean hasNext() {
+		return source.hasNext();
+	}
+
+	public PostType next() {
+		return transform.apply(source.next());
+	}
+}
