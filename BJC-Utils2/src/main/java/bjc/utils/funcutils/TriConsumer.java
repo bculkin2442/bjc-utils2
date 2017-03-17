6e4d80b6fc50abd8692652cda03a1d21e44da261
bjculkin <bjculkin@mix.wvu.edu>
2017-03-17 19:14:17 -0400

    Add new 3-arg consumer type

---
+++ BJC-Utils2/src/main/java/bjc/utils/funcutils/TriConsumer.java
@@ -0,0 +1,31 @@
+package bjc.utils.funcutils;
+
+/**
+ * Consumer that takes three arguments.
+ * 
+ * @author EVE
+ * 
+ * @param <A>
+ *                Type of the first argument.
+ * @param <B>
+ *                Type of the second argument.
+ * @param <C>
+ *                Type of the third argument.
+ *
+ */
+@FunctionalInterface
+public interface TriConsumer<A, B, C> {
+	/**
+	 * Perform the action.
+	 * 
+	 * @param a
+	 *                The first parameter.
+	 * 
+	 * @param b
+	 *                The second parameter.
+	 * 
+	 * @param c
+	 *                The third parameter.
+	 */
+	public void accept(A a, B b, C c);
+}
