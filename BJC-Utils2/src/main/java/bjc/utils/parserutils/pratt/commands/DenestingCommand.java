b9d3a95ba1886508910b3e7f25c7f9b0f735600d
bculkin2442 <bjculkin@mix.wvu.edu>
2017-03-27 23:28:42 -0400

    Minor update

--- BJC-Utils2/src/main/java/bjc/utils/parserutils/pratt/commands/DenestingCommand.java
+++ BJC-Utils2/src/main/java/bjc/utils/parserutils/pratt/commands/DenestingCommand.java
@@ -6,26 +6,40 @@ import bjc.utils.parserutils.pratt.InitialCommand;
 import bjc.utils.parserutils.pratt.ParserContext;
 import bjc.utils.parserutils.pratt.Token;
 
-public class DenestingCommand<C, V, K> extends AbstractInitialCommand<K, V, C> {
-	private InitialCommand<K, V, C> internal;
+/**
+ * A command that denests a input tree.
+ * 
+ * Useful for processing the result of passing a complex parse group to a
+ * command.
+ * 
+ * @author bjculkin
+ * 
+ * @param <K>
+ *                The key type of the tokens.
+ * 
+ * @param <V>
+ *                The value type of the tokens.
+ * 
+ * @param <C>
+ *                The state type of the parser.
+ *
+ */
+public class DenestingCommand<K, V, C> extends AbstractInitialCommand<K, V, C> {
+	private InitialCommand<K, V, C> wrapped;
 
 	/**
 	 * Create a new transforming initial command.
 	 * 
 	 * @param internal
 	 *                The initial command to delegate to.
-	 * 
-	 * @param transform
-	 *                The transform to apply to the returned tree.
 	 */
 	public DenestingCommand(InitialCommand<K, V, C> internal) {
-		super();
-		this.internal = internal;
+		wrapped = internal;
 	}
 
 	@Override
 	protected ITree<Token<K, V>> intNullDenotation(Token<K, V> operator, ParserContext<K, V, C> ctx)
 			throws ParserException {
-		return internal.denote(operator, ctx).getChild(0);
+		return wrapped.denote(operator, ctx).getChild(0);
 	}
 }
