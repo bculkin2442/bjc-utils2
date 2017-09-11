2df965cd3f5764c07a6b9e412422958462bccc20
Benjamin J. Culkin <bjculkin@mix.wvu.edu>
2017-09-11 12:13:37 -0300

    Add defines
    Defines are simple unary functions that apply replacements to strings.
    Currently, there are two types.
    * SimpleDefine - Replace all occurances of a pattern with a given
    string.
    * IteratedDefine - Replace occurances of a pattern with a string chosen
    from a series of strings.

---
+++ BJC-Utils2/src/main/java/bjc/utils/parserutils/defines/IteratedDefine.java
@@ -0,0 +1,46 @@
+package bjc.utils.parserutils.defines;
+
+import java.util.function.UnaryOperator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import bjc.utils.data.CircularIterator;
+
+public class IteratedDefine implements UnaryOperator<String> {
+	private Pattern patt;
+
+	private Iterator<String> repls;
+
+	/**
+	 * Create a new iterated define.
+	 *
+	 * @param pattern
+	 * 	The pattern to use for matching.
+	 * @param circular
+	 * 	Whether or not to loop through the list of replacers, or just
+	 * 	repeat the last one.
+	 * @param repls
+	 * 	The set of replacers to use.
+	 */
+	public IteratedDefine(Pattern pattern, boolean circular, String... replacers) {
+		patt = pattern;
+
+		repls = new CircularIterator(Arrays.asList(replacers), circular);
+	}
+
+	@Override
+	public String apply(String ln) {
+		Matcher mat     = patt.matcher(ln);
+		StringBuffer sb = new StringBuffer();
+
+		while(mat.find()) {
+			String repl = repls.next();
+
+			mat.appendReplacement(sb, repl);
+		}
+
+		mat.appendTail(sb);
+
+		return sb.toString();
+	}
+}
---
+++ BJC-Utils2/src/main/java/bjc/utils/parserutils/defines/SimpleDefine.java
@@ -0,0 +1,23 @@
+package bjc.utils.parserutils.defines;
+
+import java.util.function.UnaryOperator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SimpleDefine implements UnaryOperator<String> {
+	private Pattern patt;
+	private String  repl;
+
+	public SimpleDefine(Pattern pattern, String replace) {
+		patt = pattern;
+
+		repl = replace;
+	}
+
+	@Override
+	public String apply(String line) {
+		Matcher mat = patt.matcher(line);
+
+		return mat.replaceAll(repl);
+	}
+}
