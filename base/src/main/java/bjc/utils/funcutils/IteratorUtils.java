1e3e91f815e26d77569cea639a2cd26be21550ac
Benjamin J. Culkin <bjculkin@mix.wvu.edu>
2019-04-11 18:59:33 -0300

    ChainIterators

--- base/src/main/java/bjc/utils/funcutils/IteratorUtils.java
+++ base/src/main/java/bjc/utils/funcutils/IteratorUtils.java
@@ -10,6 +10,36 @@ import java.util.function.Function;
  *
  */
 public class IteratorUtils {
+	public static class ChainIterator<T1, T2> implements Iterator<T2> {
+		private Iterator<T1> mainItr;
+		private Function<T1, Iterator<T2>> trans;
+
+		private Iterator<T2> curItr;
+
+		public ChainIterator(Iterator<T1> mainItr, Function<T1, Iterator<T2>> trans) {
+			this.mainItr = mainItr;
+			this.trans   = trans;
+		}
+
+		@Override
+		public boolean hasNext() {
+			if (curItr != null) {
+				if (curItr.hasNext()) return true;
+				else                  return mainItr.hasNext();
+			}
+
+			return mainItr.hasNext();
+		}
+
+		@Override
+		public T2 next() {
+			if (curItr == null || !curItr.hasNext()) {
+				curItr = trans.apply(mainItr.next());
+			}
+
+			return curItr.next();
+		}
+	}
 	/**
 	 * Convert an iterator to an iterable.
 	 * 
@@ -30,5 +60,9 @@ public class IteratorUtils {
 	 */
 	public static <E> Iterator<E> I(Iterable<E> itr) {
 		return itr.iterator();
+	}
+
+	public static <A, B> Iterator<B> chain(Iterator<A> itrA, Function<A, Iterator<B>> itrB) {
+		return new ChainIterator(itrA, itrB);
 	}
 }
