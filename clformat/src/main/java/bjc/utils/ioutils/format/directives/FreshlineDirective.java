5218fb4a3cfde1568417c5190833c4c319721b5a
Benjamin J. Culkin <bjculkin@mix.wvu.edu>
2019-07-31 19:04:37 -0300

    Implement compilation for FreshlineDirective

--- clformat/src/main/java/bjc/utils/ioutils/format/directives/FreshlineDirective.java
+++ clformat/src/main/java/bjc/utils/ioutils/format/directives/FreshlineDirective.java
@@ -6,13 +6,21 @@ import bjc.utils.ioutils.format.*;
 
 /**
  * Implement the &amp; directive.
- * @author student
  *
+ * This directive will print out a specified number of newlines, and will print
+ * one less if the last thing printed was a newline.
+ *
+ * @author Ben Culkin
  */
 public class FreshlineDirective implements Directive {
-
 	@Override
 	public void format(FormatParameters dirParams) throws IOException {
+		Edict edt = compile(dirParams.toCompileCTX());
+
+		edt.format(dirParams.toFormatCTX());
+	}
+
+	public void formatF(FormatParameters dirParams) throws IOException {
 		int nTimes = 1;
 
 		CLParameters params = dirParams.getParams();
@@ -26,6 +34,38 @@ public class FreshlineDirective implements Directive {
 
 		for(int i = 0; i < nTimes; i++) {
 			dirParams.rw.write("\n");
+		}
+	}
+
+	@Override
+	public Edict compile(CompileContext compCTX) {
+		CLParameters params = compCTX.decr.parameters;
+
+		CLValue times = CLValue.nil();
+		if (params.length() >= 1) {
+			params.mapIndices("count");
+			times = params.resolveKey("count");
+		}
+
+		return new FreshlineEdict(times);
+	}
+}
+
+class FreshlineEdict implements Edict {
+	private CLValue times;
+
+	public FreshlineEdict(CLValue times) {
+		this.times = times;
+	}
+
+	@Override
+	public void format(FormatContext formCTX) throws IOException {
+		int nTimes = times.asInt(formCTX.items, "occurance count", "&", 1);
+
+		if (formCTX.writer.isLastCharNL()) nTimes -= 1;
+
+		for(int i = 0; i < nTimes; i++) {
+			formCTX.writer.write("\n");
 		}
 	}
 }
