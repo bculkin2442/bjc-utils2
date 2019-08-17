9a003b4e9a44d5685554d9f31c6249c1f087d6ad
Benjamin J. Culkin <bjculkin@mix.wvu.edu>
2019-08-17 19:23:21 -0300

    Implement compilation for TabulateDirective

--- clformat/src/main/java/bjc/utils/ioutils/format/directives/TabulateDirective.java
+++ clformat/src/main/java/bjc/utils/ioutils/format/directives/TabulateDirective.java
@@ -3,77 +3,115 @@ package bjc.utils.ioutils.format.directives;
 import java.io.*;
 
 import bjc.utils.esodata.*;
+import bjc.utils.ioutils.*;
 import bjc.utils.ioutils.format.*;
 
 public class TabulateDirective implements Directive {
 	public void format(FormatParameters dirParams) throws IOException {
-		Tape<Object> itemTape = dirParams.tParams;
-		CLModifiers mods = dirParams.getMods();
+		Edict edt = compile(dirParams.toCompileCTX());
 
-		// Support for a possible future feature
-		char padchar = ' ';
+		edt.format(dirParams.toFormatCTX());
+	}
 
-		int currCol = dirParams.rw.getLinePos();
-		if (mods.colonMod) {
-			currCol = dirParams.rw.getIndentPos();
+	@Override
+	public Edict compile(CompileContext compCTX) {
+		CLValue colinc = CLValue.nil();
+		CLValue colid = CLValue.nil();
+
+		CLParameters params = compCTX.decr.parameters;
+		CLModifiers mods = compCTX.decr.modifiers;
+
+		if (params.length() > 2) {
+			params.mapIndex("colinc", 1);
+
+			colinc = params.resolveKey("colinc");
 		}
 
+		boolean isRelative = mods.atMod;
+		boolean fromIndent = mods.colonMod;
 
-		CLParameters params = dirParams.getParams();
-		if (mods.atMod) {
-			int colrel = 1, colinc = 1;
-
-			if (params.length() > 2) {
-				params.mapIndex("colinc", 1);
-
-				colinc = params.getInt(itemTape, "colinc", "column increment", "T", 1);
-			}
-
+		if (isRelative) {
 			if (params.length() > 1) {
 				params.mapIndices("colrel");
 
-				colrel = params.getInt(itemTape, "colrel", "relative column number", "T", 1);
+				colid = params.resolveKey("colrel");
 			}
+		} else {
+			if (params.length() > 1) {
+				params.mapIndices("colnum");
+
+				colid = params.resolveKey("colnum");
+			}
+		}
+
+		return new TabulateEdict(isRelative, colinc, fromIndent, colid);
+	}
+}
+
+class TabulateEdict implements Edict {
+	private boolean isRelative;
+	private boolean fromIndent;
+
+	private CLValue colincVal;
+	private CLValue colidVal;
+
+	public TabulateEdict(boolean isRelative, CLValue colinc, boolean fromIndent, CLValue colid) {
+		this.isRelative = isRelative;
+		this.fromIndent = fromIndent;
+
+		this.colincVal = colinc;
+
+		this.colidVal  = colid;
+	}
+
+	@Override
+	public void format(FormatContext formCTX) throws IOException {
+		ReportWriter rw = formCTX.writer;
+
+		Tape<Object> itms = formCTX.items;
+
+		char padchar = ' ';
+
+		int currCol;
+
+		if (fromIndent) {
+			currCol = rw.getIndentPos();
+		} else {
+			currCol = rw.getLinePos();
+		}
+
+		if (isRelative) {
+			int colinc = colincVal.asInt(itms, "column increment", "T", 1);
+			int colrel = colidVal.asInt(itms, "relative column number", "T", 1);
 
 			for (int i = 0; i < colrel; i++) {
-				dirParams.rw.write(padchar);
+				rw.write(padchar);
 			}
 
 			int nSpaces = 0;
 
-			while ((currCol + nSpaces) % colinc != 0) nSpaces++;
+			while ((currCol + nSpaces) % colinc != 0) {
+				rw.write(padchar);
 
-			for (int i = 0; i < nSpaces; i++) {
-				dirParams.rw.write(padchar);
+				nSpaces++;
 			}
 		} else {
-			int colnum = 1, colinc = 1;
-
-			if (params.length() > 2) {
-				params.mapIndex("colinc", 1);
-
-				colinc = params.getInt(itemTape, "colinc", "column increment", "T", 1);
-			}
-
-			if (params.length() > 1) {
-				params.mapIndices("colnum");
-
-				colnum = params.getInt(itemTape, "colnum", "column number", "T", 1);
-			}
+			int colinc = colincVal.asInt(itms, "column increment", "T", 1);
+			int colnum = colidVal.asInt(itms, "column number", "T", 1);
 
 			if (currCol < colnum) {
 				for (int i = currCol; i < colnum; i++) {
-					dirParams.rw.write(padchar);
+					rw.write(padchar);
 				}
 			} else {
 				if (colinc == 0) return;
 
 				int k = 0;
 
-				while (colnum > (currCol + (k * colinc))) k++;
+				while (colnum > (currCol + (k * colinc))) {
+					rw.write(padchar);
 
-				for (int i = currCol; i < colnum; i++) {
-					dirParams.rw.write(padchar);
+					k++;
 				}
 			}
 		}
