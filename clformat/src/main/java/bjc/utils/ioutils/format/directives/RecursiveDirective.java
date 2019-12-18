aa1ad9c7aac2c53a56466388a2541ea1441f3464
bculkin2442 <bjculkin@mix.wvu.edu>
2019-12-17 20:43:43 -0500

    Clean up missing bit in RecursiveDirective

--- clformat/src/main/java/bjc/utils/ioutils/format/directives/RecursiveDirective.java
+++ clformat/src/main/java/bjc/utils/ioutils/format/directives/RecursiveDirective.java
@@ -14,45 +14,6 @@ public class RecursiveDirective implements Directive {
 		edt.format(dirParams.toFormatCTX());
 	}
 
-	public void formatF(FormatParameters dirParams) throws IOException {
-		dirParams.tParams.right();
-
-		CLFormatter.checkItem(dirParams.item, '?');
-
-		CLModifiers mods = dirParams.getMods();
-
-		if (mods.atMod) {
-			if (!(dirParams.item instanceof String))
-				throw new IllegalFormatConversionException('?', dirParams.item.getClass());
-
-			try {
-				dirParams.fmt.doFormatString((String)dirParams.item, dirParams.rw, dirParams.tParams, true);
-			} catch (EscapeException eex) {
-				if (eex.endIteration)
-					throw new UnsupportedOperationException("Colon mod not allowed on escape marker outside of iteration");
-			}
-		} else {
-			if (dirParams.tParams.atEnd())
-				throw new IllegalArgumentException("? directive requires two format parameters");
-
-			Object o = dirParams.tParams.item();
-			dirParams.tParams.right();
-
-			if (!(o instanceof Iterable<?>))
-				throw new IllegalFormatConversionException('?', o.getClass());
-
-			@SuppressWarnings("unchecked")
-			Iterable<Object> itb = (Iterable<Object>)o;
-			Tape<Object> newParams = new SingleTape<>(itb);
-
-			try {
-				dirParams.fmt.doFormatString((String)dirParams.item, dirParams.rw, newParams, true);
-			} catch (EscapeException eex) {
-				throw new UnsupportedOperationException("Colon mod not allowed on escape marker outside of iteration");
-			}
-		}
-	}
-	
 	@Override
 	public Edict compile(CompileContext compCTX) {
 		return new RecursiveEdict(compCTX.decr.modifiers.atMod, compCTX.formatter);
@@ -114,6 +75,7 @@ class RecursiveEdict implements Edict {
 			try {
 				String bod = (String)body;
 
+				// :DynamicString
 				fmt.doFormatString(bod, formCTX.writer, newParams, true);
 			} catch (EscapeException eex) {
 				throw new UnexpectedColonEscape();
